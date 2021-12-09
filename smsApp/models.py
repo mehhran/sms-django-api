9d2862977343cf27a27d82ec990bf58c372313cd
meh jav <mehhran@gmail.com>
2021-12-09 14:38:43 +0330

    models

--- smsApp/models.py
+++ smsApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Sms(models.Model):
+    sender = models.CharField(max_length=100)
+    body = models.TextField()
+    datetime = models.DateTimeField(auto_now=True)
+    receiver = models.CharField(max_length=100)
+
+class BatteryLevel(models.Model):
+    batteryPct = models.FloatField()
+    action = models.CharField(max_length=100)
+    action_time = models.DateTimeField()
